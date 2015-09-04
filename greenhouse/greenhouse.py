fe278f61de0d100adefcfe799e03cb1e682fc3c3
Daniel Grech <daniel.grech@vivant.com.au>
2015-09-04 11:40:42 +1000

    Initial about page

--- greenhouse/greenhouse.py
+++ greenhouse/greenhouse.py
@@ -14,6 +14,10 @@ app = Flask(__name__)
 def index():
 	return app.send_static_file('index.html')
 
+@app.route("/about")
+def about():
+	return app.send_static_file('about.html')
+
 @app.route('/grow', methods=['POST'])
 def generate():
 	output_dir = "/tmp/" + str(random.randint(1, sys.maxint))
