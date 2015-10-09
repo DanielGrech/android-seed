0a9fafe14d22a067416c3671a83282322a3f73a1
Daniel Grech <daniel.grech@vivant.com.au>
2015-10-09 12:46:47 +1100

    Read port from env var

--- greenhouse/greenhouse.py
+++ greenhouse/greenhouse.py
@@ -55,4 +55,5 @@ def generate():
 			os.remove(output_file_name)
 
 if __name__ == "__main__":
-    application.run()
+	port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
